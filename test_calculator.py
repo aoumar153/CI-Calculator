ac38c692692e9b0bdcf49b3b0005212d6b9e157f
Abdullah Oumar <oumarab@oregonstate.edu>
2021-06-07 20:38:31 -0700

    fixed white spaces again

--- test_calculator.py
+++ test_calculator.py
@@ -13,7 +13,7 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 5 == calculator.subtract(10, 5)
-    
+
     def test_multiply(self):
         assert 4 == calculator.multiply(2, 2)
 
